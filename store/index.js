25b4388c850da86fb712d617b0f940a6a3ba58d5
abedigram <mbapost@yahoo.com>
2019-10-14 11:56:19 +0330

    firebase fetch data implemented

--- store/index.js
+++ store/index.js
@@ -1,56 +1,39 @@
-    import Vuex from 'vuex'
+import Vuex from 'vuex';
+import axios from 'axios';
 
-    const createStore = () => {
-        return new Vuex.Store({
-            state: {
-                loadedPosts: []
-            },
-            mutations: {
-                setPosts(state, posts) {
-                    state.loadedPosts = posts
-                }
-            },
-            actions: {
-                nuxtServerInit(vuexContext, context) {
-                    if(!process.client) {
-                        console.log(context.req.session);
-                    }
-                    return new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            vuexContext.commit('setPosts',
-                                [
-                                  {
-                                    id: "1",
-                                    title: "First Post",
-                                    previewText: "This is our first post!",
-                                    thumbnail:
-                                      "https://picsum.photos/400/200"
-                                  },
-                                  {
-                                    id: "2",
-                                    title: "Second Post",
-                                    previewText: "This is our second post!",
-                                    thumbnail:
-                                      "https://picsum.photos/400/200"
-                                  }
-                                ])  
-                            resolve();
-                              // reject(new Error())
-                            }, 1000);
-                        }); 
-
-                },
-                
-                setPosts(vuexContext, posts) {
-                    vuexContext.commit('setPosts', posts); //commit a mutation
-                }
-            },
-            getters: {
-                loadedPosts(state){
-                    return state.loadedPosts
-                }
+const createStore = () => {
+    return new Vuex.Store({
+        state: {
+            loadedPosts: []
+        },
+        mutations: {
+            setPosts(state, posts) {
+                state.loadedPosts = posts
             }
-        })
-    };
+        },
+        actions: {
+            nuxtServerInit(vuexContext, context) {
+                return axios.get('https://knocks-town.firebaseio.com/posts.json')
+                    .then(res => {
+                        const postsArray = [];
+                        for (const key in res.data) {
+                            postsArray.push({...res.data[key], id: key });
+                        }
+                        vuexContext.commit('setPosts', postsArray);
+                    })
+                    .catch(e => context.error);
+            },
 
-    export default createStore;
+            setPosts(vuexContext, posts) {
+                vuexContext.commit('setPosts', posts); //commit a mutation
+            }
+        },
+        getters: {
+            loadedPosts(state) {
+                return state.loadedPosts
+            }
+        }
+    })
+};
+
+export default createStore;
